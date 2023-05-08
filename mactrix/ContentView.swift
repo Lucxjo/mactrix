378c04b1afb4eaa6f62a9befd3fdd50961ba6309
Louis Hollingworth <louis@hollingworth.ch>
2023-05-08 16:16:04 +0100

    Initial commit
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

---
+++ mactrix/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mactrix
+//
+//  Created by Louis Hollingworth on 2023-05-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mactrix/mactrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  mactrixApp.swift
+//  mactrix
+//
+//  Created by Louis Hollingworth on 2023-05-08.
+//
+
+import SwiftUI
+
+@main
+struct mactrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
